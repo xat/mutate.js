27f71017b24ea3ff40a164d2658e884f30b79331
Simon Kusterer <simon@soped.com>
2014-09-15 08:36:56 +0200

    no more amd support and code reformating

--- mutate.js
+++ mutate.js
@@ -1,64 +1,58 @@
-!function(glob) {
+!function (glob) {
 
-    var mutate = function(input, ctx) {
-        var map = {};
+  var mutate = function (input, ctx) {
+    var map = {};
 
-        return {
+    return {
 
-            // add a new method
-            method: function(sig, fn) {
-                sig || (sig = []);
-                map[sig.join('::').toLowerCase()] = {
-                    fn: fn,
-                    inject: fn.length > sig.length
-                };
-
-                return this;
-            },
-
-            // return the composed new function
-            close: function() {
-                return function() {
-                    var args = Array.prototype.slice.call(arguments, 0),
-                        sig = (function() {
-                            var ret = [];
-                            for (var i = 0, len = args.length; i < len; i++) {
-                                ret.push(toType(args[i]));
-                            }
-                            return ret;
-                        })().join('::');
-
-                    if (map[sig]) {
-                        if (map[sig].inject) args.unshift(input);
-                        return map[sig].fn.apply(ctx || map[sig].fn, args);
-                    }
-
-                    return input && input.apply(ctx || input, args);
-                }
-            }
-
+      // add a new method
+      method: function (sig, fn) {
+        sig || (sig = []);
+        map[sig.join('::').toLowerCase()] = {
+          fn: fn,
+          inject: fn.length > sig.length
         };
 
+        return this;
+      },
+
+      // return the composed new function
+      close: function () {
+        return function () {
+          var args = Array.prototype.slice.call(arguments, 0),
+            sig = (function () {
+              var ret = [];
+              for (var i = 0, len = args.length; i < len; i++) {
+                ret.push(toType(args[i]));
+              }
+              return ret;
+            })().join('::');
+
+          if (map[sig]) {
+            if (map[sig].inject) args.unshift(input);
+            return map[sig].fn.apply(ctx || map[sig].fn, args);
+          }
+
+          return input && input.apply(ctx || input, args);
+        }
+      }
+
     };
 
-    // http://javascriptweblog.wordpress.com/2011/08/08/fixing-the-javascript-typeof-operator/
-    var toType = function(obj) {
-        return Object.prototype.toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
-    };
+  };
 
-    // Node.js / browserify
-    if (typeof module !== 'undefined' && module.exports) {
-        module.exports = mutate;
-    }
-    // AMD
-    else if (typeof define !== 'undefined' && define.amd) {
-        define([], function () {
-            return mutate;
-        });
-    }
-    // <script>
-    else {
-        glob.mutate = mutate;
-    }
+  // http://javascriptweblog.wordpress.com/2011/08/08/fixing-the-javascript-typeof-operator/
+  var toType = function (obj) {
+    return Object.prototype.toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
+  };
+
+  // Node.js / browserify
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = mutate;
+  }
+  // <script>
+  else {
+    glob.mutate = mutate;
+  }
 
 }(this);
