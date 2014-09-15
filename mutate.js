8a30b59a28fadd76397f3ed70d4f97b7688eea55
Simon Kusterer <simon@soped.com>
2014-09-15 09:21:10 +0200

    allow an array to be passed in as second argument of 'method'.

--- mutate.js
+++ mutate.js
@@ -7,10 +7,12 @@
 
       // add a new method
       method: function (sig, fn) {
+        var isArr = toType(fn) === 'array';
         sig || (sig = []);
+
         map[sig.join('::').toLowerCase()] = {
-          fn: fn,
-          inject: fn.length > sig.length
+          fn: isArr ? passObject(fn) : fn,
+          inject: isArr ? true : (fn.length > sig.length)
         };
 
         return this;
@@ -19,26 +21,37 @@
       // return the composed new function
       close: function () {
         return function () {
-          var args = Array.prototype.slice.call(arguments, 0),
-            sig = (function () {
-              var ret = [];
-              for (var i = 0, len = args.length; i < len; i++) {
-                ret.push(toType(args[i]));
-              }
-              return ret;
-            })().join('::');
+          var args = Array.prototype.slice.call(arguments, 0);
+          var sig = (function () {
+            var ret = [];
+            for (var i = 0, len = args.length; i < len; i++) {
+              ret.push(toType(args[i]));
+            }
+            return ret;
+          })().join('::');
 
           if (map[sig]) {
             if (map[sig].inject) args.unshift(input);
-            return map[sig].fn.apply(ctx || map[sig].fn, args);
+            return map[sig].fn.apply(ctx || null, args);
           }
 
-          return input && input.apply(ctx || input, args);
+          return input && input.apply(ctx || null, args);
         }
       }
 
     };
 
+  };
+
+  var passObject = function (props) {
+    return function(done) {
+      var args = Array.prototype.slice.call(arguments, 1);
+      var obj = {};
+      for (var i = 0, len = props.length; i < len; i++) {
+        obj[props[i]] = args[i];
+      }
+      return done(obj);
+    };
   };
 
   // http://javascriptweblog.wordpress.com/2011/08/08/fixing-the-javascript-typeof-operator/
